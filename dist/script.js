b3e2b440ca6f513f4b1f2cbe61ca965248648511
Nikhil Sharma <nikhilsharma181104@gmail.com>
2025-10-21 21:14:25 +0530

    feat: enhance styles and functionality
    - Added new color variables for blue and indigo shades in output.css.
    - Introduced additional spacing and text size variables.
    - Improved utility classes for positioning, margins, paddings, and grid layouts.
    - Updated JavaScript to streamline the intersection observer logic and added click event handling for dt elements.
    - Refined CSS for base styles, including adjustments to container margins and hover effects.
    - Implemented gradient hover effects for specific elements.

--- dist/script.js
+++ dist/script.js
@@ -1,46 +1,59 @@
 const navDialog = document.getElementById('nav-dialog');
 function handleMenu() {
-  navDialog.classList.toggle('hidden');
+    navDialog.classList.toggle('hidden');
 }
 
-const initialTranslateLTR = -46 * 4;
-const initialTranslateRTL = 36 * 4;
+const initialTranslateLTR = -48*4;
+const initialTranslateRTL = 36*4;
 
 function setupIntersectionObserver(element, isLTR, speed) {
-  function scrollHandler() {
-    const translateX =
-      (window.innerHeight - element.getBoundingClientRect().top) * speed;
+    const intersectionCallback = (entries) => {
+        const isIntersecting = entries[0].isIntersecting;
+        if(isIntersecting) {
+            document.addEventListener('scroll', scrollHandler);
+        } else {
+            document.removeEventListener('scroll', scrollHandler);
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
 
-    let totalTranslate = 0;
-    if (isLTR) {
-      totalTranslate = translateX + initialTranslateLTR;
-    } else {
-      totalTranslate = -(translateX + initialTranslateRTL);
+    intersectionObserver.observe(element);
+
+    function scrollHandler() {
+        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+
+        let totalTranslate = 0;
+        if(isLTR) {
+            totalTranslate = translateX + initialTranslateLTR;
+        } else {
+            totalTranslate = -(translateX + initialTranslateRTL);
+        }
+
+        element.style.transform = `translateX(${totalTranslate}px)`;
     }
 
-    element.style.transform = `translateX(${totalTranslate}px)`;
-  }
-
-  const intersectionCallback = (entries) => {
-    const isIntersecting = entries[0].isIntersecting;
-    if (isIntersecting) {
-      document.addEventListener("scroll", scrollHandler);
-    } else {
-      document.removeEventListener("scroll", scrollHandler);
-    }
-  };
-
-  const observer = new IntersectionObserver(intersectionCallback);
-  observer.observe(element);
 }
 
-const line1 = document.getElementById("line1");
-const line2 = document.getElementById("line2");
-const line3 = document.getElementById("line3");
-const line4 = document.getElementById("line4");
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+const line4 = document.getElementById('line4');
+
 
 setupIntersectionObserver(line1, true, 0.15);
 setupIntersectionObserver(line2, false, 0.15);
 setupIntersectionObserver(line3, true, 0.15);
 
 setupIntersectionObserver(line4, true, 0.8);
+
+const dtElements = document.querySelectorAll('dt');
+dtElements.forEach(element => {
+    element.addEventListener('click', () => {
+        const ddId = element.getAttribute('aria-controls');
+        const ddElement = document.getElementById(ddId);
+        const ddArrowIcon = element.querySelectorAll('i')[0];
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+    })
+})
